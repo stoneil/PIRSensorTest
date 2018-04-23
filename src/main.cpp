43ebc6e5e819c2ba9237fabc1d32837ce7d9cd7a
stoneil <seanogon@gmail.com>
2018-04-23 02:10:03 -0400

    inits movement as false and sets the userLED to off when movement is set to false

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,38 @@
 
 #define pirPin PF_13
 #define userButtonPin PC_13
+#define UserLEDPin PB_0
 
 Serial pc(USBTX,USBRX);
 Timer timer;
 DigitalIn inputPIR(pirPin);
 DigitalIn userButton(userButtonPin);
+DigitalOut UserLED(UserLEDPin);
 Timer t;
 
-bool movement;
+bool movement = false;
 bool hasPrinted = false;
 
 void pirHigh();
 void serialTest();
+void setLedWithMovement();
 
 int main() {
   // serialTest();
   // inputPIR.rise(&pirHigh);
   t.start();
   timer.start();
+  setLedWithMovement();
   while(1){
       //printf("%f\n",t.read());
+
+      setLedWithMovement();
 
       // Periodically sends the updated status of whether there is a person there or not
       if (timer.read() > 1.0) {
         printf("%d\n", movement);
         timer.reset();
       }
-
 
       if (inputPIR.read() == 1) {
         t.reset();
@@ -51,8 +56,11 @@ int main() {
 //   printf("There is movement!\n");
 // }
 
-void sendMessage(){
-
+void setLedWithMovement(){
+  if (movement)
+    UserLED.write(1);
+  else
+    UserLED.write(0);
 }
 
 
